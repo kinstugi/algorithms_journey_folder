8d20c1402268846846ac970f41e390077a3a81d5
baffour.frimpong <baffour.frimpong@vero-digital.solutions>
2025-01-15 12:18:59 +0300

    paint_a_strip

---
+++ codeforces/a/paint_a_strip.cpp
@@ -0,0 +1,63 @@
+// C++ template for AtCoder created by nazfox
+
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+using i32 = int;
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using f32 = float;
+using f64 = double;
+using str = string;
+template <typename T> using vec = vector<T>;
+template <typename T> using vvec = vec<vec<T> >;
+#define rep(i, n) for (i32 i = 0; i < (n); i++)
+#define rrep(i, n) for (i32 i = (n) - 1; i >= 0; i--)
+#define each(e, v) for (auto& e : v)
+#define itr(it, v) for (auto it = (v).begin(); it != (v).end(); it++)
+#define ritr(it, v) for (auto it = (v).rbegin(); it != (v).rend(); it++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define debug(x) cerr << "L" << __LINE__ << ": " << #x << " = " << (x) << endl;
+template <typename T> inline T min(vector<T>& v){return *min_element(all(v));}
+template <typename T> inline T max(vector<T>& v){return *max_element(all(v));}
+template <typename T> inline T argmin(vector<T>& v){return (min_element(all(v)) - v.begin());}
+template <typename T> inline T argmax(vector<T>& v){return (max_element(all(v)) - v.begin());}
+template <typename T> inline bool chmin(T& a, const T& b){bool c=a>b;if(c)a=b;return c;}
+template <typename T> inline bool chmax(T& a, const T& b){bool c=a<b;if(c)a=b;return c;}
+template <typename T> inline void sort(vector<T>& v){sort(all(v));}
+template <typename T> inline void rsort(vector<T>& v){sort(rall(v));}
+template <typename T> inline void reverse(vector<T>& v){reverse(all(v));}
+
+int main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    // freopen("sample.in", "r", stdin);
+
+    i32 t, n;
+    cin >> t;
+
+    while(t--){
+        cin >> n;
+       for (int ans = 1, cur = 1; ; ans++, cur = cur*2 + 2){
+        if (cur >= n){
+            cout << ans << "\n";
+            break;
+        }
+       } 
+    }
+    return 0;
+}
