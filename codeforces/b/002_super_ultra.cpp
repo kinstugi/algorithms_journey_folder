d78f64feec239ac5826b50d81420e6c5c968c5c6
baffour.frimpong <baffour.frimpong@vero-digital.solutions>
2025-01-17 23:31:28 +0300

    super ultra

---
+++ codeforces/b/002_super_ultra.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("sample.in", "r", stdin);
+
+    int t, n;
+    cin >> t;
+    while (t--){
+        cin >> n;
+        vector<int> evens;
+    }
+    return 0;
+}
